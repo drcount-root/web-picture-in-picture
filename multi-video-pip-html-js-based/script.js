b8c96f1baa167707b3f32921840c6819dbb4e324
SubhamSvelteTech <subham.sveltetech@gmail.com>
2024-03-05 11:10:21 +0530

    multi video in single canvas

--- multi-video-pip-html-js-based/script.js
+++ multi-video-pip-html-js-based/script.js
@@ -7,21 +7,38 @@ const ctx = canvas.getContext("2d");
 
 const mainVideos = document.querySelectorAll(".main_video");
 
-// const ctxPip = async () => {
-//   const pipWindow = await documentPictureInPicture?.requestWindow();
-//   return pipWindow;
-// };
+const ctxPip = async () => {
+  const pipWindow = await documentPictureInPicture?.requestWindow();
+  return pipWindow;
+};
 
 const enterPip = async () => {
   const pipWindow = await documentPictureInPicture.requestWindow();
   mainVideos[0].play().then(() => {
     const drawFrame = () => {
-      ctx.drawImage(mainVideos[0], 0, 0, canvas.width, canvas.height);
+      ctx.drawImage(mainVideos[0], 0, 0, canvas.width / 2, canvas.height);
       requestAnimationFrame(drawFrame);
     };
 
     drawFrame();
   });
+
+  mainVideos[1].play().then(() => {
+    const drawFrame = () => {
+      ctx.drawImage(
+        mainVideos[1],
+        canvas.width / 2,
+        0,
+        canvas.width / 2,
+        canvas.height
+      );
+      requestAnimationFrame(drawFrame);
+    };
+
+    drawFrame();
+  });
+
+  document?.body?.append(canvas);
 
   pipWindow.document.body.append(canvas);
 };
